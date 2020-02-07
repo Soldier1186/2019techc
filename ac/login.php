681c67b2b1070a281cb5df2ec45c45314d24bc39
Oha_Dirty Gorilla <38026687+Soldier1186@users.noreply.github.com>
2020-02-08 01:48:22 +0900

    Update login.php

--- ac/login.php
+++ ac/login.php
@@ -1,47 +1,13 @@
 <?php
 $dbh = new PDO('mysql:host=database-1.c6bncgbidtab.us-east-1.rds.amazonaws.com;dbname=keijiban','Oha','password');
-session_start();
-if (!empty($_SESSION['id']))
-{
-	header('Location: index.php');
-}
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-	$name = $_POST['name'];
-	$password = $_POST['password'];
-	$errors = array();
-	if ($name == '')  // もしnameが空だったら
-    {
-        $errors['name'] = '※ ユーザネームが未入力です';
-    }
-    if ($password == '')  
-    {
-        $errors['password'] = '※ パスワードが未入力です';
-    }
-    // バリデーション突破後
-    if (empty($errors))  // $errorsが空だったら(=エラーが無かったら)
-    {
-        $sql = 'SELECT password FROM users WHERE name = :name;';  // テーブルの中に該当レコードがあるか
-        $sth = $dbh->prepare($sql);
-        $sth->bindParam(":name", $_POST['name']);
-        $sth->execute();
-        $row = $sth->fetch();  // レコードの取り出し
-        // var_dump($row);
-    if ($row)  // 該当レコードがあったら
-    {
-        $_SESSION['id'] = $row['id'];  // セッションのidにレコードのidを持たせる
-        $_SESSION['name'] = $row['name'];
-        header('Location: index.php');  // index.phpに飛ばす
-        exit;
-    }
-    else  // もし該当レコードがなかったら
-    {
-        echo 'ユーザネームかパスワードが間違っています';
-    }
-    }
-}
-?>
 
+if($_POST['name'] != null && $_POST['password'] != null){
+	$name     = $_POST['name'];
+	$password = $_POST['password'];
+	$sql = 'SELECT * FROM users WHERE name = :name';
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute(':name' => $name);
+?>
 <!DOCTYPE html>
 <html lang="ja">
     <head>
@@ -50,21 +16,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     </head>
     <body>
         <h1>ログイン</h1>
-        <form aciton="" method="post">
-            <p>
-            ユーザネーム: <input type="text" name="name">
-            <?php if ($errors['name']) : ?>
-                <?php echo h($errors['name']) ?>
-            <?php endif ?>
-            </p>
-            <p>
-            パスワード: <input type="password" name="password">
-            <?php if ($errors['password']) : ?>
-                <?php echo h($errors['password']) ?>
-            <?php endif ?>
-            </p>
-            <input type="submit" value="ログイン">
-        </form>
+        <form method="POST" action="login.php">
+		ID:<input type="text" name ="name"><br />
+		PW:<input type="text> name="password"><br />
+		<input type="submit" value="ログイン">
+	</form>
         <a href="edit.php">新規登録はこちら!</a>
     </body>
 </html>
